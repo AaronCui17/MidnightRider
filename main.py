3318b35df7ac80dd1e906a86ef1e13b2751f315e
Aaron Cui <aaroncui17@gmail.com>
2021-01-07 09:48:16 -0800

    Done status check

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ REACH THE END BEFORE THE MAN GON GET U.
 
 CHOICES =   """
     ----
+    D.
+    E. Status Check
     Q. QUIT
     ----    
 """
@@ -39,24 +41,40 @@ def intro():
     time.sleep(1)
 
 def main():
-    intro()
+    #intro()
 
     # Variables
     done = False
+    km_travelled = 0        # 100km is the end
+    agents_distance = -20   # 0 is the end
+    turns = 0
+    tofu = 3                # 3 is max
+    fuel = 50               # max is 50
+    hunger = 0
 
     # MAIN LOOP
     while not done:
-        pass
         # TODO: Check if reached END GAME
 
         # TODO: Present the user their choices
         print(CHOICES)
 
         user_choice = input("What do you want to do? ").lower().strip("!,.? ")
-
-        if user_choice == "q":
+        if user_choice == "d"
+            pass
+            #
+        elif user_choice == "e":
+            print(f"\t---Status Check---")
+            print(f"\tkm travelled: {km_travelled}")
+            print(f"\tFuel remaining: {fuel} L")
+            print(f"\tAgents are {abs(agents_distance)} kms behind")
+            print(f"\tYou have {tofu} tofu left")
+            print(f"\t--------\n")
+        elif user_choice == "q":
             print("Thanks for playing g you a real one :)")
             done = True
+
+        time.sleep(1.5)
 
         # TODO: Change the environment based on
         #       user choice, and RNG
